f315cfe236cfcfa5d5069b6d1a7b81aa7d90e5a5
Daniel Yount <dyountmusic@icloud.com>
2019-09-30 21:50:59 -0600

    Initial Commit

---
+++ Pulsr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pulsr
+//
+//  Created by Daniel Yount on 9/30/19.
+//  Copyright © 2019 Daniel Yount. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
